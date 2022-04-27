7b0cdddda6e121315055e9d2991fb12a854e209a
Dan Forder <danfrdr@gmail.com>
2022-04-27 12:57:15 +0100

    add color palette

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import confetti from "canvas-confetti";
 
 const randomInRange = (min, max) => Math.random() * (max - min) + min;
 
-const fireConfetti = () => {
+const fireConfetti = (colors) => {
   for (let index = 0; index < 5; index++) {
     const confettiOptions = {
       particleCount: randomInRange(50, 100),
       angle: randomInRange(55, 125),
       spread: randomInRange(50, 70),
       origin: { y: 0.6 },
+      colors,
     };
 
     confetti(confettiOptions);
@@ -50,11 +51,15 @@ const colorThief = new ColorThief();
 
 const onImageLoad = () => {
   const [red, green, blue] = colorThief.getColor(image);
+  const colorPalette = colorThief.getPalette(image);
+
+  // set body to dominant color
   const body = document.querySelector("body");
   body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
+  // fire confetti with color palette
   console.log("%cTHIS IS THE COLOR", `color: rgb(${red},${green},${blue});`);
-  fireConfetti();
+  fireConfetti(colorPalette);
 };
 
 image.addEventListener("load", onImageLoad);
