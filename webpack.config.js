d5ec704d70891d5857c4815b8bd8f28715fb7e93
Dan Forder <danfrdr@gmail.com>
2022-04-27 17:48:24 +0100

    index.ks -> main.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   mode: "production",
   output: {
     filename: "main.js",
